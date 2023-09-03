62115efe3c9f592ef854b2029601b270f1769b55
LeeJungBum <jungbum05114@gmail.com>
2023-09-04 03:07:41 +0900

    feat: Customer.pickFoods메소드

--- src/main/java/com/nhnacademy/mart/Customer.java
+++ src/main/java/com/nhnacademy/mart/Customer.java
@@ -1,6 +1,10 @@
 package com.nhnacademy.mart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Customer {
+    private static final Logger logger = LoggerFactory.getLogger(Customer.class);
 
     // 고객 구매 목록
     private final BuyList buyList;
@@ -10,7 +14,7 @@ public class Customer {
 
     public Customer(BuyList buyList) {
         this.buyList = buyList;
-    }
+}
 
     // 장바구니 챙기기
     public void bring(Basket basket) {
@@ -18,7 +22,14 @@ public class Customer {
     }
 
     // TODO pickFoods 메서드 구현
+    public void pickFoods(FoodStand foodStand) {
+        logger.info(buyList.toString());
+        for (BuyList.Item item:buyList.getItems())
+        {
+            basket.add(foodStand.putBasket(item.getName()));
+        }
 
+    }
 
     // TODO payTox 메서드 구현
 
